0a5114eeee315cfb425dedd3bd3d79e3be39b0c3
Rajan Manickam <rajanmanickam23@gmail.com>
2024-08-25 12:07:19 +0530

    Create DatabaseConnection.java

---
+++ BusReservationSystem/Src/DatabaseConnection.java
@@ -0,0 +1,19 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/bus_reservation";
+    private static final String USER = "root";
+    private static final String PASSWORD = "Enter_your_password"; // Change as per your MySQL configuration
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+}
